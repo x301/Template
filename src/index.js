49bde7189ff595aa5f364a177382d5873ce8864a
Sergey <ishellyp@gmail.com>
2018-04-21 08:53:02 +0300

    Add sass

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
+import './style.sass';
 function component() {
     var element = document.createElement('div');
   
      // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+           {
+            test: /\.(scss|sass)$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }
+      ]
+    },
+ // plugins: [
+    //new ExtractTextPlugin({
+    // filename: 'style.css',
+    // publicPath: './src'
+   // })
+  //]   
 };
